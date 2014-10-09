4a63ed1a6f8e9e3a5289c94153779046121340b7
Jessie Keck <jessie.keck@gmail.com>
2014-10-09 13:43:48 -0700

    Validate on viewer registration that viewer has not already been registered w/ the same supported type.

--- spec/lib/embed_spec.rb
+++ spec/lib/embed_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
-class TestViewerClass; end
+class TestViewerClass
+  def self.supported_types
+    [:test_view]
+  end
+end
 
 describe Embed do
   describe 'registering viewers' do
@@ -12,6 +16,9 @@ describe Embed do
       Embed.register_viewer(TestViewerClass)
       expect(Embed.registered_viewers).to be_a(Array)
       expect(Embed.registered_viewers).to include TestViewerClass
+    end
+    it 'should raise an error if a viewer registers itself w/ a supported type that is already registered' do
+      expect( ->{ Embed.register_viewer(Embed.registered_viewers.first) } ).to raise_error(Embed::DuplicateViewerRegistration)
     end
   end
 end
