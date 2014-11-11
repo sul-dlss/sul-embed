55a856e51e58b878282336c11b1713a0fc5f1969
Jessie Keck <jessie.keck@gmail.com>
2014-11-11 11:00:01 -0800

    Namespace embed form labels so we don't accidentally focus on outside form elements.

--- spec/features/embed_this_panel_spec.rb
+++ spec/features/embed_this_panel_spec.rb
@@ -19,17 +19,17 @@ describe 'embed this panel', js: true do
     it 'should have the form elements for updating the embed code' do
       page.find('[data-toggle="sul-embed-embed-this-panel"]', match: :first).click
       expect(page).to have_css('.sul-embed-options-label', text: 'SELECT OPTIONS:')
-      expect(page).to have_css('input#title[type="checkbox"]')
-      expect(page).to have_css('input#search[type="checkbox"]')
-      expect(page).to have_css('input#embed[type="checkbox"]')
-      expect(page).to have_css('textarea#iframe-code')
+      expect(page).to have_css('input#sul-embed-embed-title[type="checkbox"]')
+      expect(page).to have_css('input#sul-embed-embed-search[type="checkbox"]')
+      expect(page).to have_css('input#sul-embed-embed[type="checkbox"]')
+      expect(page).to have_css('textarea#sul-embed-iframe-code')
     end
     it 'should update the textarea when the checkboxes are selected' do
       page.find('[data-toggle="sul-embed-embed-this-panel"]', match: :first).click
       expect(page.find('.sul-embed-embed-this-panel textarea').value).to match /src='\S+\/iframe\?url=http:\/\/purl\.stanford\.edu\/ab123cd4567'/
-      page.find("input#search[type='checkbox']").trigger('click')
+      page.find("input#sul-embed-embed-search[type='checkbox']").trigger('click')
       expect(page.find('.sul-embed-embed-this-panel textarea').value).to match /src='\S+\/iframe\?url=http:\/\/purl\.stanford\.edu\/ab123cd4567&hide_search=true'/
-      page.find("input#search[type='checkbox']").trigger('click')
+      page.find("input#sul-embed-embed-search[type='checkbox']").trigger('click')
       expect(page.find('.sul-embed-embed-this-panel textarea').value).to match /src='\S+\/iframe\?url=http:\/\/purl\.stanford\.edu\/ab123cd4567'/
     end
   end
@@ -37,7 +37,7 @@ describe 'embed this panel', js: true do
     let(:spec_fixture) { image_purl }
     it 'should include the form elements for downloading an image' do
       page.find('[data-toggle="sul-embed-embed-this-panel"]', match: :first).click
-      expect(page).to have_css('input#download[type="checkbox"]')
+      expect(page).to have_css('input#sul-embed-embed-download[type="checkbox"]')
     end
   end
 end
