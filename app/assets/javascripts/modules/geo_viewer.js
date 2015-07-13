18a40f642b984885abda864466dafcb12ea6910c
Jack Reed <pjreed@stanford.edu>
2015-07-13 16:21:30 -0700

    adds WMS layer preview for public layers

--- app/assets/javascripts/modules/geo_viewer.js
+++ app/assets/javascripts/modules/geo_viewer.js
@@ -2,11 +2,18 @@
 (function( global ) {
   'use strict';
   var Module = (function() {
+    var dataAttributes;
+    var map;
+    
+    var isDefined = function(object) {
+       return typeof object !== 'undefined';
+    };
+
     return {
       init: function() {
-        var boundingBox = jQuery('#sul-embed-geo-map').data().boundingBox;
+        dataAttributes = jQuery('#sul-embed-geo-map').data();
 
-        var map = L.map('sul-embed-geo-map').fitBounds(boundingBox);
+        map = L.map('sul-embed-geo-map').fitBounds(dataAttributes.boundingBox);
 
         L.tileLayer(
             'https://otile{s}-s.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
@@ -19,8 +26,25 @@
               subdomains: '1234'
             }
         ).addTo(map);
-        L.rectangle(boundingBox, {color: "#0000FF", weight: 4}).addTo(map)
+        
+        Module.addVisualizationLayer();
         map.invalidateSize();
+      },
+      addVisualizationLayer: function() {
+        var hasWmsUrl = isDefined(dataAttributes.wmsUrl);
+        var hasLayers = isDefined(dataAttributes.layers);
+
+        if (hasWmsUrl && hasLayers) {
+          L.tileLayer.wms(dataAttributes.wmsUrl, {
+              layers: dataAttributes.layers,
+              format: 'image/png',
+              transparent: true,
+              tiled: true
+          }).addTo(map);
+        } else {
+          L.rectangle(dataAttributes.boundingBox, {color: '#0000FF', weight: 4})
+            .addTo(map);
+        }
       }
     };
   })();
--- spec/lib/embed/viewer/geo_spec.rb
+++ spec/lib/embed/viewer/geo_spec.rb
@@ -23,8 +23,8 @@ describe Embed::Viewer::Geo do
   end
 
   describe '.body_html' do
-    it 'should return geo html body' do
-      stub_purl_response_and_request(geo_purl, request)         
+    it 'should return geo html body for public resources' do
+      stub_purl_response_and_request(geo_purl_public, request)
       expect(geo_viewer).to receive(:asset_host).at_least(:twice).and_return('http://example.com/')
 
       html = Capybara.string(geo_viewer.to_html)
@@ -33,6 +33,8 @@ describe Embed::Viewer::Geo do
       expect(html).to have_css '#sul-embed-geo-map', visible: false
       expect(html).to have_css('#sul-embed-geo-map[style="height: 400px"]', visible: false)
       expect(html).to have_css('#sul-embed-geo-map[data-bounding-box=\'[["38.298673", "-123.387626"], ["39.399103", "-122.528843"]]\']', visible: false)
+      expect(html).to have_css('#sul-embed-geo-map[data-wms-url="https://geowebservices.stanford.edu/geoserver/wms/"]', visible: false)
+      expect(html).to have_css('#sul-embed-geo-map[data-layers="druid:12345"]', visible: false)
     end
   end
   describe '.external_url' do
@@ -51,7 +53,7 @@ describe Embed::Viewer::Geo do
       expect(geo_viewer.download_html).to eq ''
     end
     it 'generates a file list when file has resources' do
-      stub_purl_response_and_request(geo_purl, request)
+      stub_purl_response_and_request(geo_purl_public, request)
       html = Capybara.string(geo_viewer.download_html)
       expect(html).to have_css 'li', visible: false, count: 1
       expect(html).to have_css 'a[href="https://stacks.stanford.edu/file/druid:12345/data.zip"]', visible: false
@@ -60,6 +62,27 @@ describe Embed::Viewer::Geo do
       stub_purl_response_and_request(stanford_restricted_file_purl, request)
       html = Capybara.string(geo_viewer.download_html)
       expect(html).to have_css 'li.sul-embed-stanford-only', visible: false
+    end
+  end
+  describe '#map_element_options' do
+    it 'for public content' do
+      stub_purl_response_and_request(geo_purl_public, request)
+      expect(geo_viewer.map_element_options).to be_an Hash
+      expect(geo_viewer.map_element_options).to include style: 'height: 400px'
+      expect(geo_viewer.map_element_options).to include id: 'sul-embed-geo-map'
+      expect(geo_viewer.map_element_options).to include 'data-bounding-box' => [['38.298673', '-123.387626'], ['39.399103', '-122.528843']]
+      expect(geo_viewer.map_element_options).to include 'data-wms-url' => 'https://geowebservices.stanford.edu/geoserver/wms/'
+      expect(geo_viewer.map_element_options).to include 'data-layers' => 'druid:12345'
+    end
+
+    it 'for restricted content' do
+      stub_purl_response_and_request(geo_purl_restricted, request)
+      expect(geo_viewer.map_element_options).to be_an Hash
+      expect(geo_viewer.map_element_options).to include style: 'height: 400px'
+      expect(geo_viewer.map_element_options).to include id: 'sul-embed-geo-map'
+      expect(geo_viewer.map_element_options).to include 'data-bounding-box' => [['38.298673', '-123.387626'], ['39.399103', '-122.528843']]
+      expect(geo_viewer.map_element_options).to_not include 'data-layers'
+      expect(geo_viewer.map_element_options).to_not include 'data-wms-url'
     end
   end
 end
