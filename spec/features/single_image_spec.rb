c5c300b1d2e4c321aec0c01b259922c8b920257d
Jack Reed <pjreed@stanford.edu>
2015-09-25 14:27:19 -0700

    add more conditions for when left/right controls should be shown

--- spec/features/single_image_spec.rb
+++ spec/features/single_image_spec.rb
@@ -17,6 +17,8 @@ describe 'image x viewer', js: true do
         expect(page).to_not have_css '[data-sul-view-perspective]'
       end
       expect(page).to_not have_css '.sul-embed-image-x-thumb-slider-container'
+      expect(page).to_not have_css '.sul-i-arrow-right-8'
+      expect(page).to_not have_css '.sul-i-arrow-left-8'
     end
   end
   describe 'multi image viewer (not paged)' do
@@ -24,12 +26,14 @@ describe 'image x viewer', js: true do
       fill_in_default_sandbox_form('fw090jw3474')
       click_button 'Embed'
     end
-    it 'adds overview perspective and individuals mode' do
+    it 'adds overview perspective, individuals mode, and left/right controls' do
       within '.sul-embed-header' do
         expect(page).to have_css '[data-sul-view-perspective]'
         expect(page).to have_css '[data-sul-view-mode="individuals"]'
         expect(page).to_not have_css '[data-sul-view-mode="paged"]'
       end
+      expect(page).to have_css '.sul-i-arrow-right-8'
+      expect(page).to have_css '.sul-i-arrow-left-8'
     end
   end
   describe 'multimage viewer (paged)' do
@@ -37,7 +41,7 @@ describe 'image x viewer', js: true do
       fill_in_default_sandbox_form('cy496ky1984')
       click_button 'Embed'
     end
-    it 'adds overview perspective, paged mode, and individuals mode' do
+    it 'adds overview perspective, paged mode, individuals mode, and left/right controls' do
       within '.sul-embed-header' do
         expect(page).to have_css '[data-sul-view-perspective]'
         expect(page).to have_css '[data-sul-view-mode="individuals"]'
