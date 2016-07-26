a96a1933631955c5a61e6281c11233de1467403b
Naomi Dushay <ndushay@stanford.edu>
2016-07-26 16:55:13 -0700

    add test for download panel not showing for file viewer

--- spec/features/download_panel_spec.rb
+++ spec/features/download_panel_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe 'download panel', js: true do
   include PURLFixtures
   let(:request) { Embed::Request.new(url: 'https://purl.stanford.edu/ab123cd4567') }
+  it 'not shown for file viewer' do
+    stub_purl_response_with_fixture(multi_file_purl)
+    visit_iframe_response
+    expect(page).to have_css '.sul-embed-body.sul-embed-file'
+    expect(page).not_to have_css '.sul-embed-download-panel'
+  end
   describe 'toggle button' do
     before do
       stub_purl_response_with_fixture(image_purl)
