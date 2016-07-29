f5ce93da04f60639a90b9b3111ad138a871b4f35
Naomi Dushay <ndushay@stanford.edu>
2016-07-29 16:33:59 -0700

    add more urls to is_it_working

--- config/initializers/is_it_working.rb
+++ config/initializers/is_it_working.rb
@@ -2,4 +2,8 @@ Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
   h.check :url, get: Settings.purl_url
   h.check :url, get: Settings.stacks_url
   h.check :url, get: Settings.geo_wms_url
+  # check geo_external_url without the last part after the slash
+  pieces = Settings.geo_external_url.split('/')
+  h.check :url, get: pieces[0..-2].join('/')
+  h.check :url, get: Settings.was_thumbs_url
 end
