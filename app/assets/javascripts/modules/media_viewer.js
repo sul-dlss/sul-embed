430ae550c028762879d95a9e14872576b90b7733
Tommy Ingulfsen <tommyi@stanford.edu>
2016-09-09 16:53:22 -0700

    Force the use of Flash when the browser is IE or Edge on Windows 10.

--- app/assets/javascripts/modules/media_viewer.js
+++ app/assets/javascripts/modules/media_viewer.js
@@ -1,6 +1,7 @@
 //= require modules/thumb_slider
 //= require video.js/video.js
 //= require videojs-contrib-hls/videojs-contrib-hls.js
+//= require client.js/client.min.js
 /*global ThumbSlider */
 /*global videojs */
 
@@ -146,11 +147,23 @@
       return hlsSource.prop('src').indexOf('/mp3:') >= 0;
     }
 
+    function ms10Browser() {
+      var fingerprinter = new ClientJS();
+      var browser = fingerprinter.getBrowser();
+      return (((browser.toLowerCase() === 'edge') || (browser.toLowerCase() === 'ie')) &&
+               fingerprinter.getOS().toLowerCase() === 'windows' &&
+               fingerprinter.getOSVersion().toLowerCase() === '10');
+    }
+
     // We must use flash for MP3s on browsers that do not support HLS natively.
     // This is a simple boolean function that can easily tell us if the current
-    // browser does not support HLS and the media is an MP3
+    // browser does not support HLS and the media is an MP3. We also use flash
+    // on Windows 10 Microsoft browsers (IE and Edge) because of a known issue
+    // with the VideoJS HLS plugin. See
+    // https://github.com/videojs/videojs-contrib-hls/issues/833  
     function mustUseFlash(mediaObject) {
-      return (!canPlayHLS() && mediaObjectIsMP3(mediaObject));
+      return (ms10Browser() ||
+              (!canPlayHLS() && mediaObjectIsMP3(mediaObject)));
     }
 
     // Remove the HLS source for MP3 files if the browser
