689f544bdfb8ead2fe85a1ef889f1b0ff9b75df2
edsilv <e.silverton@gmail.com>
2017-03-22 23:02:55 +0000

    use public directory instead of gem

--- Gemfile
+++ Gemfile
@@ -75,4 +75,3 @@ gem 'sul_styles', '~>0.6'
 gem 'okcomputer'
 
 gem 'rake', '~>11'
-gem 'pul_uv_rails', git: 'https://github.com/pulibrary/pul_uv_rails.git'
--- lib/embed/viewer/uv.rb
+++ lib/embed/viewer/uv.rb
@@ -7,7 +7,7 @@ module Embed
           <div class='sul-embed-body' style='height: #{body_height}px' data-sul-embed-theme='#{asset_url("uv.css")}'>
             <div class='uv' data-uri='#{manifest_json_url}' data-locale="en-GB:English" data-config='#{config_url}' style='height: #{body_height}px; width:100%'></div>
             <script src='#{asset_url("uv.js")}'></script>
-            #{::PulUvRails::UniversalViewer.script_tag}
+            <script id='embedUV' src='#{embed_url}'></script>
           </div>
           HTML
       end
@@ -21,6 +21,10 @@ module Embed
       # we want to change this
       def config_url
         '/uv-config.json'
+      end
+
+      def embed_url
+        '/uv-2.0.1/lib/embed.js'
       end
 
       def manifest_json_url
