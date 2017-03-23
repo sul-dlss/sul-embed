f4d4a69e9e881e1e5de522599f880e3a3cfab497
Jessie Keck <jessie.keck@gmail.com>
2017-03-22 19:41:12 -0700

    Add SULEmbed's local uv.js via jQuery.getScript.

--- lib/embed/viewer/uv.rb
+++ lib/embed/viewer/uv.rb
@@ -6,7 +6,9 @@ module Embed
           <<-HTML.strip_heredoc
           <div class='sul-embed-body' style='height: #{body_height}px' data-sul-embed-theme='#{asset_url("uv.css")}'>
             <div class='uv' data-uri='#{manifest_json_url}' data-locale="en-GB:English" data-config='#{config_url}' style='height: #{body_height}px; width:100%'></div>
-            <script src='#{asset_url("uv.js")}'></script>
+            <script>
+              ;jQuery.getScript("#{asset_url('uv.js')}");
+            </script>
             <script id='embedUV' src='#{embed_url}'></script>
           </div>
           HTML
