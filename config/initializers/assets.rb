353f38575f01768b70363b78d985f094692cd7b7
Chris Beer <chris@cbeer.info>
2017-06-02 10:39:56 -0700

    Only precompile application assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
-Rails.application.config.assets.precompile += ['*.js', '*.css']
+Rails.application.config.assets.precompile += Dir.glob(Rails.root + 'app' + 'assets' + 'javascripts' + '*.js').map { |x| File.basename(x) }
+Rails.application.config.assets.precompile += Dir.glob(Rails.root + 'app' + 'assets' + 'stylesheets' + '*.scss*').map { |x| File.basename(x) }
