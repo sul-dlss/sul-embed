d1b31107a7f09ed296e3551cfc3bb02f6f94980b
Chris Beer <chris@cbeer.info>
2017-07-05 11:20:45 -0700

    hack all uv extensions to fix requirejs problem

--- public/uv-3/lib/uv-virtex-extension-dependencies.js
+++
@@ -1,6 +0,0 @@
-define(function() {
-    return {
-        sync: ['three.min', 'draco_decoder'],
-        async: ['VRControls', 'VREffect', 'stats.min', 'DRACOLoader', 'GLTFLoader', 'Detector', 'WebVR', 'virtex', 'iiif-metadata-component']
-    };
-});
