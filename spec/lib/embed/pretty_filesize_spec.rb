6cb51df621e22baa53492b6f1837b49bc210e5a7
Justin Coyne <jcoyne@justincoyne.com>
2017-07-07 10:08:31 -0500

    Put the underscore where the comma goes

--- spec/lib/embed/pretty_filesize_spec.rb
+++ spec/lib/embed/pretty_filesize_spec.rb
@@ -4,7 +4,7 @@ describe Embed::PrettyFilesize do
   let(:dummy_class) { Class.new { include Embed::PrettyFilesize }.new }
   describe '.pretty_file_size' do
     it 'should return the correct size' do
-      expect(dummy_class.pretty_filesize(123_45)).to eq '12.35 kB'
+      expect(dummy_class.pretty_filesize(12_345)).to eq '12.35 kB'
     end
   end
 end
