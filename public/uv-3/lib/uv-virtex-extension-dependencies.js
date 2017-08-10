6f71c584dbb36e14222e64d563c19f7a5e69a42b
Jack Reed <phillipjreed@gmail.com>
2017-08-10 15:11:36 -0400

    remove UV requirejs hack

---
+++ public/uv-3/lib/uv-virtex-extension-dependencies.js
@@ -0,0 +1,6 @@
+define(function() {
+    return {
+        sync: ['three.min', 'draco_decoder'],
+        async: ['VRControls', 'VREffect', 'stats.min', 'DRACOLoader', 'GLTFLoader', 'Detector', 'WebVR', 'virtex', 'iiif-metadata-component']
+    };
+});
