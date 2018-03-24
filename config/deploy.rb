ae4a41d21a2466a0f40197c19bf20e0ac7ca7ff2
Chris Beer <cabeer@stanford.edu>
2018-03-24 07:43:29 -0700

    Always deploy from the master branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'https://github.com/sul-dlss/sul-embed.git'
 set :user, 'embed'
 
 # Default branch is :master
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, 'master'
 
 # Default value for :scm is :git
 # set :scm, :git
