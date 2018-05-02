5be3a813f73fe89743b6497cb7ec9a5553e2214a
Chris Beer <cabeer@stanford.edu>
2018-05-02 07:57:05 -0700

    Capybara 3.x's interpretation of visible for our stubbed images is different than before

--- spec/features/file_list_accessibility_spec.rb
+++ spec/features/file_list_accessibility_spec.rb
@@ -5,14 +5,14 @@ describe 'file list accessibility', js: true do
   it 'page has relevant sr-only classes' do
     stub_purl_response_with_fixture(hybrid_object_purl)
     visit_iframe_response
-    expect(page).to have_css 'img.sul-embed-square-image[alt]'
+    expect(page).to have_css 'img.sul-embed-square-image[alt]', visible: false
     expect(page).to have_content 'Download item 1'
     expect(page).to have_content "Preview\nitem 1"
     expect(page).to have_css 'a[aria-expanded="false"]'
     expect(page).to have_css 'div.sul-embed-preview[aria-hidden="true"]', visible: false
     expect(page).to have_css 'div.sul-embed-item-count[aria-live="polite"]'
     click_link 'Preview'
-    expect(page).to have_css '.sul-embed-preview img[alt]'
+    expect(page).to have_css '.sul-embed-preview img[alt]', visible: false
     expect(page).to have_content "Close preview\nitem 1"
     expect(page).to have_css 'a[aria-expanded="true"]'
     expect(page).to have_css 'div.sul-embed-preview[aria-hidden="false"]'
--- spec/features/file_preview_spec.rb
+++ spec/features/file_preview_spec.rb
@@ -10,20 +10,20 @@ describe 'file preview', js: true do
     click_link('Preview')
     expect(page).to have_css('.sul-embed-preview', visible: true)
     expect(page).to have_css('.sul-embed-preview-toggle', count: 1, text: 'Close preview')
-    expect(page).to have_css('.sul-embed-preview img', count: 1)
+    expect(page).to have_css('.sul-embed-preview img', count: 1, visible: false)
   end
   it 'displays sul-embed-square-image for file list icon' do
     stub_purl_response_with_fixture(hybrid_object_purl)
     visit_iframe_response('ab123cd4567')
     click_link('Preview')
-    expect(page).to have_css('.sul-embed-square-image')
-    expect(page).to have_css('img[src="https://stacks.stanford.edu/image/iiif/ab123cd4567%2Ftn629pk3948_img_1/square/100,100/0/default.jpg"]')
+    expect(page).to have_css('.sul-embed-square-image', visible: false)
+    expect(page).to have_css('img[src="https://stacks.stanford.edu/image/iiif/ab123cd4567%2Ftn629pk3948_img_1/square/100,100/0/default.jpg"]', visible: false)
   end
   it 'has (hidden) thumb image' do
     stub_purl_response_with_fixture(hybrid_object_purl)
     visit_iframe_response('ab123cd4567')
     click_link('Preview')
     expect(page).to have_css('.sul-embed-preview')
-    expect(page).to have_css('img[src="https://stacks.stanford.edu/image/iiif/ab123cd4567%2Ftn629pk3948_img_1/full/!400,400/0/default.jpg"]')
+    expect(page).to have_css('img[src="https://stacks.stanford.edu/image/iiif/ab123cd4567%2Ftn629pk3948_img_1/full/!400,400/0/default.jpg"]', visible: false)
   end
 end
