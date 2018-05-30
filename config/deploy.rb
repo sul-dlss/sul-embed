54f78f06a9ddb34986e2ec59a569a2a323c15f2f
Chris Beer <cabeer@stanford.edu>
2018-05-30 11:57:43 -0700

    Mark config/newrelic.yml as a capistrano shared file

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w(config/honeybadger.yml)
+set :linked_files, %w(config/honeybadger.yml config/newrelic.yml)
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w(bin config/settings log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
