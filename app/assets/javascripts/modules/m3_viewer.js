403f1a527126a1f03df1ef2ec449511ce52f709b
Jack Reed <phillipjreed@gmail.com>
2019-04-22 17:58:01 -0600

    Update Mirador3 to 033ffba8c69ba171aeafe9262a9d3ad666b1548f

--- app/assets/javascripts/modules/m3_viewer.js
+++ app/assets/javascripts/modules/m3_viewer.js
@@ -9,11 +9,23 @@
         var data = $el.data();
         var miradorViewer = Mirador.viewer({
           id: 'sul-embed-m3',
-          theme: {
-            palette: {
-              type: 'dark',
-              lightened: {
-                dark: '#5f574f'
+          
+          selectedTheme: 'stanford',
+          themes: {
+            stanford: {
+              palette: {
+                type: 'dark',
+                primary: {
+                  main: '#610e0e',
+                },
+                secondary: {
+                  main: '#610e0e',
+                },
+                shades: {
+                  dark: '#000000',
+                  main: '#5f574f',
+                  light: '#616161',
+                }
               }
             }
           },
@@ -22,6 +34,12 @@
               loadedManifest: data.m3Uri
             }
           ],
+          window: {
+            allowClose: false,
+            allowMaximize: false,
+            hideAnnotationsPanel: true,
+            sideBarOpenByDefault: true
+          },
           workspaceControlPanel: {
             enabled: false
           }
