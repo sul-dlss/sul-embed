4ad0e66c89eded5fe76540e553d12fe1b2dfc2a5
Jack Reed <phillipjreed@gmail.com>
2019-06-06 13:47:09 -0600

    Setup Mirador viewer to use webpacker

---
+++ app/javascript/packs/m3.js
@@ -0,0 +1,6 @@
+import { CssInjection } from '../../assets/javascripts/modules/css_injection.js';
+import { CommonViewerBehavior } from '../../assets/javascripts/modules/common_viewer_behavior.js';
+import M3Viewer from '../../assets/javascripts/modules/m3_viewer.js';
+
+CssInjection.appendToHead();
+CommonViewerBehavior.initializeViewer(M3Viewer.init);
