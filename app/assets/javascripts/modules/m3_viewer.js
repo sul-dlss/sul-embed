3c56e73c87f9f6546b079305f8d9e2ea11fdfa60
Chris Beer <cabeer@stanford.edu>
2019-06-26 13:59:10 -0700

    Add polyfills and refactor the M3Viewer to use IE11-compatible code

--- app/assets/javascripts/modules/m3_viewer.js
+++ app/assets/javascripts/modules/m3_viewer.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import Mirador from 'mirador/dist/es/src/index.js';
 import miradorShareDialogPlugin from 'mirador-share-plugin/es/MiradorShareDialog.js';
 import miradorSharePlugin from 'mirador-share-plugin/es/miradorSharePlugin.js';
@@ -5,94 +7,94 @@ import miradorDownloadPlugin from 'mirador-dl-plugin/es/miradorDownloadPlugin.js
 import miradorDownloadDialogPlugin from 'mirador-dl-plugin/es/MiradorDownloadDialog.js';
 import osdReferencePlugin from 'mirador-dl-plugin/es/OSDReferences.js';
 
-export default class M3Viewer {
-  static init() {
-    var $el = jQuery('#sul-embed-m3');
-    var data = $el.data();
+export default {
+      init: function() {
+        var $el = jQuery('#sul-embed-m3');
+        var data = $el.data();
 
-    // Determine which panel should be open
-    var sideBarPanel = 'info';
-    if (data.search) {
-      sideBarPanel = 'search';
-    }
-    if (data.showAttribution) {
-      sideBarPanel = 'attribution';
-    }
+        // Determine which panel should be open
+        var sideBarPanel = 'info';
+        if (data.search) {
+          sideBarPanel = 'search';
+        }
+        if (data.showAttribution) {
+          sideBarPanel = 'attribution';
+        }
 
-    Mirador.viewer({
-      id: 'sul-embed-m3',
-      miradorDownloadPlugin: {
-        restrictDownloadOnSizeDefinition: true,
-      },
-      miradorSharePlugin: {
-        embedOption: {
-          enabled: true,
-          embedUrlReplacePattern: [
-            /.*\.edu\/(\w+)\/iiif\/manifest/,
-            'https://embed.stanford.edu/iframe?url=https://purl.stanford.edu/$1',
+        Mirador.viewer({
+          id: 'sul-embed-m3',
+          miradorDownloadPlugin: {
+            restrictDownloadOnSizeDefinition: true,
+          },
+          miradorSharePlugin: {
+            embedOption: {
+              enabled: true,
+              embedUrlReplacePattern: [
+                /.*\.edu\/(\w+)\/iiif\/manifest/,
+                'https://embed.stanford.edu/iframe?url=https://purl.stanford.edu/$1',
+              ],
+            },
+            dragAndDropInfoLink: 'https://library.stanford.edu/projects/international-image-interoperability-framework/viewers',
+            shareLink: {
+              enabled: true,
+              manifestIdReplacePattern: [
+                /\/iiif\/manifest/,
+                '',
+              ],
+            },
+          },
+          selectedTheme: 'sul',
+          themes: {
+            sul: {
+              palette: {
+                type: 'light',
+                primary: {
+                  main: '#8c1515',
+                },
+                secondary: {
+                  main: '#8c1515',
+                },
+                shades: {
+                  dark: '#2e2d29',
+                  main: '#ffffff',
+                  light: '#f4f4f4',
+                },
+                notification: {
+                  main: '#e98300'
+                },
+              }
+            }
+          },
+          windows: [
+            {
+              defaultSearchQuery: data.search.length > 0 ? data.search : undefined,
+              suggestedSearches: data.suggestedSearch.length > 0 ? [data.suggestedSearch] : null,
+              loadedManifest: data.m3Uri
+            }
           ],
-        },
-        dragAndDropInfoLink: 'https://library.stanford.edu/projects/international-image-interoperability-framework/viewers',
-        shareLink: {
-          enabled: true,
-          manifestIdReplacePattern: [
-            /\/iiif\/manifest/,
-            '',
-          ],
-        },
-      },
-      selectedTheme: 'sul',
-      themes: {
-        sul: {
-          palette: {
-            type: 'light',
-            primary: {
-              main: '#8c1515',
-            },
-            secondary: {
-              main: '#8c1515',
-            },
-            shades: {
-              dark: '#2e2d29',
-              main: '#ffffff',
-              light: '#f4f4f4',
-            },
-            notification: {
-              main: '#e98300'
-            },
+          window: {
+            allowClose: false,
+            allowFullscreen: true,
+            allowMaximize: false,
+            authNewWindowCenter: 'screen',
+            defaultSideBarPanel: sideBarPanel,
+            hideAnnotationsPanel: true,
+            hideSearchPanel: false,
+            hideWindowTitle: (data.hideTitle === true),
+            sideBarOpenByDefault: (data.showAttribution === true || data.search.length > 0)
+          },
+          workspace: {
+            showZoomControls: true,
+          },
+          workspaceControlPanel: {
+            enabled: false,
           }
-        }
-      },
-      windows: [
-        {
-          defaultSearchQuery: data.search.length > 0 ? data.search : undefined,
-          suggestedSearches: data.suggestedSearch.length > 0 ? [data.suggestedSearch] : null,
-          loadedManifest: data.m3Uri
-        }
-      ],
-      window: {
-        allowClose: false,
-        allowFullscreen: true,
-        allowMaximize: false,
-        authNewWindowCenter: 'screen',
-        defaultSideBarPanel: sideBarPanel,
-        hideAnnotationsPanel: true,
-        hideSearchPanel: false,
-        hideWindowTitle: (data.hideTitle === true),
-        sideBarOpenByDefault: (data.showAttribution === true || data.search.length > 0)
-      },
-      workspace: {
-        showZoomControls: true,
-      },
-      workspaceControlPanel: {
-        enabled: false,
+        }, [
+          miradorSharePlugin,
+          miradorShareDialogPlugin,
+          osdReferencePlugin,
+          miradorDownloadDialogPlugin,
+          miradorDownloadPlugin,
+        ]);
       }
-    }, [
-      miradorSharePlugin,
-      miradorShareDialogPlugin,
-      osdReferencePlugin,
-      miradorDownloadDialogPlugin,
-      miradorDownloadPlugin,
-    ]);
-  }
-}
+    };
