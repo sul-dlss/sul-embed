ea2eb48fa4c24e507c405afbdd44769d3e29513b
Jessie Keck <jessie.keck@gmail.com>
2019-08-07 08:55:49 -0700

    Use Fullscreen module in the PDF viewer.

--- app/javascript/packs/pdf_viewer.js
+++ app/javascript/packs/pdf_viewer.js
@@ -3,6 +3,7 @@ import { EmbedThis } from '../../assets/javascripts/modules/embed_this.js';
 import { PopupPanels } from '../../assets/javascripts/modules/popup_panels.js';
 import { CommonViewerBehavior } from '../../assets/javascripts/modules/common_viewer_behavior.js';
 import PDFViewer from '../../assets/javascripts/modules/pdf_viewer.js';
+import Fullscreen from '../../assets/javascripts/modules/fullscreen.js';
 
 CssInjection.injectFontIcons();
 CssInjection.appendToHead();
@@ -10,3 +11,4 @@ CommonViewerBehavior.initializeViewer();
 PopupPanels.init();
 EmbedThis.init();
 PDFViewer.init();
+Fullscreen.init('.sul-embed-pdf');
