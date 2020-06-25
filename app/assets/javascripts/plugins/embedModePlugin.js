b46ddd6d4009c96312d37cca0ba53ec5fad49b5f
Chris Beer <cabeer@stanford.edu>
2020-06-25 07:03:56 -0700

    Add a non-drag and drop way to compare manifests

--- app/assets/javascripts/plugins/embedModePlugin.js
+++ app/assets/javascripts/plugins/embedModePlugin.js
@@ -1,4 +1,11 @@
-import { AllowWindowTopBarCloseForNonMainWindows, AllowDragAndDrop } from '../components/embedMode';
+import {
+  AllowWindowTopBarCloseForNonMainWindows,
+  AllowDragAndDrop,
+  CompareDialog,
+  CompareMenuItem,
+} from '../components/embedMode';
+import * as actions from 'mirador/dist/es/src/state/actions/index.js';
+import { getWindow } from 'mirador/dist/es/src/state/selectors';
 
 export default [
   {
@@ -10,5 +17,27 @@ export default [
     component: AllowDragAndDrop,
     mode: 'wrap',
     target: 'Workspace',
+  },
+  {
+    component: CompareMenuItem,
+    mapDispatchToProps: {
+      updateWindow: actions.updateWindow,
+    },
+    mode: 'add',
+    name: 'CompareMenuItem',
+    target: 'WindowTopBarPluginMenu',
+  },
+  {
+    component: CompareDialog,
+    mapDispatchToProps: {
+      addWindow: actions.addWindow,
+      updateWindow: actions.updateWindow,
+    },
+    mapStateToProps: (state, { windowId }) => ({
+      show: getWindow(state, { windowId }).showCompareDialog,
+    }),
+    mode: 'add',
+    name: 'CompareDialog',
+    target: 'Window',
   },
 ];
