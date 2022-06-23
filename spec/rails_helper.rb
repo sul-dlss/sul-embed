f22701cc2d38f7a38bcdfe991017ae76f88b5e2f
Nick Budak <budak@stanford.edu>
2022-06-23 11:58:18 -0700

    Run headful Chrome via xvfb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,16 +11,15 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require 'selenium-webdriver'
 
-Capybara.register_driver :selenium_chrome_headless do |app|
+Capybara.register_driver :selenium_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
   options.add_argument('--ignore-certificate-errors')
   options.add_argument('--window-size=1280,720')
   options.add_argument('--disable-dev-shm-usage')
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.javascript_driver = :selenium_chrome
 
 Capybara.default_max_wait_time = ENV['CI'] ? 30 : 10
 
