c3854a5ee2630d6b3c5206d79fd336ff8c4f24ad
Justin Coyne <jcoyne@justincoyne.com>
2023-01-30 21:47:25 -0600

    Enable logging of rspec status
    So we can use --only-failures when testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
