b7760f7bd33a8b2c87686c93a083f3cf2a3fa495
Justin Coyne <jcoyne@justincoyne.com>
2023-01-30 22:12:19 -0600

    Use webpacker to build the file entrypoint
    So that we have a consistent asset pipeline for all the entrypoints

---
+++ app/javascript/packs/file.js
@@ -0,0 +1,18 @@
+import { CssInjection } from '../../assets/javascripts/modules/css_injection';
+import { EmbedThis } from '../../assets/javascripts/modules/embed_this';
+import { PopupPanels } from '../../assets/javascripts/modules/popup_panels';
+import { DownloadAll } from '../../assets/javascripts/modules/download_all';
+import { FilePreview } from '../../assets/javascripts/modules/file_preview';
+import CommonViewerBehavior from '../../assets/javascripts/modules/common_viewer_behavior';
+
+import '../../assets/javascripts/vendor/tooltip';
+
+document.addEventListener('DOMContentLoaded', () => {
+  CssInjection.injectFontIcons();
+  CssInjection.appendToHead();
+  CommonViewerBehavior.initializeViewer();
+  FilePreview.init();
+  PopupPanels.init();
+  EmbedThis.init();
+  DownloadAll.init();
+})
