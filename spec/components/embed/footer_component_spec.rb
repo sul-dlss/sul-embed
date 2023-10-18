aa857950eaa627b9304f1ae869047bef99e11585
Justin Coyne <jcoyne@justincoyne.com>
2023-10-17 19:29:02 -0500

    Extract footer to a component

--- spec/components/embed/footer_component_spec.rb
+++ spec/components/embed/footer_component_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'embed/footer/_generic' do
+RSpec.describe Embed::FooterComponent, type: :component do
   include PurlFixtures
 
   let(:request) { Embed::Request.new(url: 'http://purl.stanford.edu/abc123') }
@@ -10,28 +10,26 @@ RSpec.describe 'embed/footer/_generic' do
   let(:viewer) { Embed::Viewer::Geo.new(request) }
 
   before do
-    view.lookup_context.view_paths.push 'app/views/embed'
     allow(request).to receive(:purl_object).and_return(object)
     allow(object).to receive(:response).and_return(image_purl)
-    allow(view).to receive(:viewer).and_return(viewer)
-    render
+    render_inline(described_class.new(viewer:))
   end
 
   it "returns the object's footer" do
-    expect(rendered).to have_css 'div.sul-embed-footer'
-    expect(rendered).to have_css '[aria-label="open embed this panel"]'
-    expect(rendered).to have_css '[aria-label="open download panel"]'
+    expect(page).to have_css 'div.sul-embed-footer'
+    expect(page).to have_css '[aria-label="open embed this panel"]'
+    expect(page).to have_css '[aria-label="open download panel"]'
   end
 
   describe 'fullscreen button' do
     context 'when the viewer is configured to use the local fullscreen function' do
       let(:viewer) { Embed::Viewer::PdfViewer.new(request) }
 
-      it { expect(rendered).to have_css('button#full-screen-button') }
+      it { expect(page).to have_css('button#full-screen-button') }
     end
 
     context 'when the viewer is not configured to use the local fullscreen function' do
-      it { expect(rendered).not_to have_css('button#full-screen-button') }
+      it { expect(page).not_to have_css('button#full-screen-button') }
     end
   end
 end
