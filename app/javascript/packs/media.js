a1e239876cbb60088827a01b9a9cb2ed8d7f60c7
Justin Coyne <jcoyne@justincoyne.com>
2023-10-24 09:45:13 -0500

    React media stub

--- app/javascript/packs/media.js
+++ app/javascript/packs/media.js
@@ -11,6 +11,24 @@ import { CssInjection } from '../src/modules/css_injection.js';
 // import { PopupPanels } from '../src/modules/popup_panels.js';
 import CommonViewerBehavior from '../src/modules/common_viewer_behavior.js';
 
+import { createRoot } from 'react-dom/client';
+
+import Button from '@mui/material/Button';
+
+export default function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+function NavigationBar() {
+  // TODO: Actually implement a navigation bar
+  return <h1>Hello from React!</h1>;
+}
+
+const domNode = document.querySelector('.media-component');
+const root = createRoot(domNode);
+root.render(<ButtonUsage />);
+
+
 $(document).ready(function() {
   CssInjection.injectFontIcons();
   CssInjection.appendToHead();
