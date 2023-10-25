6d92de378fb291109f4f5b3e9c0bb61423023a93
Justin Coyne <jcoyne@justincoyne.com>
2023-10-24 22:08:12 -0500

    Remove unused test support method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,11 +59,6 @@ def toggle_download_panel
   page.find('[data-sul-embed-toggle="sul-embed-download-panel"]', match: :first).click
 end
 
-def toggle_metadata_panel
-  page.find('button[data-sul-embed-toggle="sul-embed-metadata-panel"]', visible: true)
-  page.find('[data-sul-embed-toggle="sul-embed-metadata-panel"]', match: :first).click
-end
-
 def fill_in_default_sandbox_form(druid = 'ab123cd4567')
   fill_in 'api-endpoint', with: embed_path
   fill_in 'url-scheme', with: "http://purl.stanford.edu/#{druid}"
