0b4b52908b2880579819c3a4836c63b63c2e45f2
Justin Coyne <jcoyne@justincoyne.com>
2023-10-26 15:58:09 -0500

    Add stimulus

--- app/javascript/packs/media.js
+++ app/javascript/packs/media.js
@@ -1,4 +1,10 @@
-'use strict';
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("../src/controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
+
 
 import { CssInjection } from '../src/modules/css_injection.js';
 import { EmbedThis } from '../src/modules/embed_this.js';
