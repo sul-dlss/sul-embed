8aef9ecf865d38f93ae264a9aaef3407f44746fb
Justin Coyne <jcoyne@justincoyne.com>
2023-10-28 15:22:54 -0500

    Add a button for the right drawer

--- app/javascript/src/controllers/media_controller.js
+++ app/javascript/src/controllers/media_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "leftDrawer" ]
+  static targets = [ "leftDrawer", "rightDrawer" ]
 
   toggleLeft() {
     this.leftDrawerTarget.classList.toggle('open')
+  }
+
+  toggleRight() {
+    this.rightDrawerTarget.classList.toggle('open')
   }
 }
