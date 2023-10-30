526fcf0f30674bb98b955ea0572d3967449f54eb
Justin Coyne <jcoyne@justincoyne.com>
2023-10-30 10:27:32 -0500

    Cleanup unused code

--- app/components/embed_panel_component.rb
+++ app/components/embed_panel_component.rb
@@ -5,20 +5,8 @@ class EmbedPanelComponent < ViewComponent::Base
     @viewer = viewer
   end
 
-  delegate :height, :width, :request, :iframe_title, :purl_object, to: :viewer
+  delegate :request, :purl_object, to: :viewer
   delegate :title, to: :purl_object, prefix: true
-  delegate :druid, to: :purl_object
+
   attr_reader :viewer
-
-  def iframe_component
-    IframeComponent.new(viewer:)
-  end
-
-  def width_style
-    width ? "#{width}px" : '100%'
-  end
-
-  def height_style
-    request.fullheight? ? '100%' : "#{height}px"
-  end
 end
