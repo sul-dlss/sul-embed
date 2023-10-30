d662ea54fd1e65c132e4720ec2afe65e04b7deed
Justin Coyne <jcoyne@justincoyne.com>
2023-10-30 14:17:34 -0500

    Add buttons for different sections within the drawers

--- app/javascript/src/controllers/media_controller.js
+++ app/javascript/src/controllers/media_controller.js
@@ -1,13 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "leftDrawer", "rightDrawer" ]
-
+  static targets = [ "leftDrawer", "rightDrawer", "metadata", "share", "contents", "downloads" ]
+  connect() {
+    this.metadataTarget.hidden = false
+  }
+  
   toggleLeft() {
     this.leftDrawerTarget.classList.toggle('open')
   }
 
-  toggleRight() {
-    this.rightDrawerTarget.classList.toggle('open')
+  displayDownloads() {
+    this.rightDrawerTarget.classList.add('open')
+    this.downloadsTarget.hidden = false
+    this.contentsTarget.hidden = true
+  }
+
+  displayContents() {
+    this.rightDrawerTarget.classList.add('open')
+    this.downloadsTarget.hidden = true
+    this.contentsTarget.hidden = false
+  }
+
+  displayMetadata() {
+    this.metadataTarget.hidden = false
+    this.shareTarget.hidden = true
+  }
+
+  displayShare() {
+    this.metadataTarget.hidden = true
+    this.shareTarget.hidden = false
   }
 }
