1cc6acbf99b607c6852a5baed8884f6417402fa9
Justin Coyne <jcoyne@justincoyne.com>
2023-10-30 14:32:05 -0500

    Add active state for buttons in the left drawer

--- app/javascript/src/controllers/media_controller.js
+++ app/javascript/src/controllers/media_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "leftDrawer", "rightDrawer", "metadata", "share", "contents", "downloads" ]
+  static targets = [ "leftDrawer", "leftButton", "rightDrawer", "metadata", "share", "contents", "downloads" ]
   connect() {
     this.metadataTarget.hidden = false
   }
@@ -22,12 +22,18 @@ export default class extends Controller {
     this.contentsTarget.hidden = false
   }
 
-  displayMetadata() {
+  displayMetadata(evt) {
+    this.leftButtonTargets.forEach((target) => target.classList.remove('active'))
+    // The evt.target may be the SVG, so need to look for a button
+    evt.target.closest('button').classList.add('active')
     this.metadataTarget.hidden = false
     this.shareTarget.hidden = true
   }
 
-  displayShare() {
+  displayShare(evt) {
+    this.leftButtonTargets.forEach((target) => target.classList.remove('active'))
+    // The evt.target may be the SVG, so need to look for a button
+    evt.target.closest('button').classList.add('active')
     this.metadataTarget.hidden = true
     this.shareTarget.hidden = false
   }
