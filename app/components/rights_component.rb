c758463defe351b4042db8063d1200d2af47b033
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-11-22 12:41:15 -0800

    Enhance rights panel in left companion window of media player
    Fixes #1797
    
    This commit makes two changes to the rights panel in the LCW of the media
    player, both of which bring look & feel parity with the Mirador viewer:
    
    * Add the Stanford Libraries logo to the bottom of the panel
    * When an object lacks rights information (use statement, copyright statement,
    license), render a default attribution

--- app/components/rights_component.rb
+++ app/components/rights_component.rb
@@ -9,4 +9,20 @@ class RightsComponent < ViewComponent::Base
 
   delegate :purl_object, to: :viewer
   delegate :use_and_reproduction, :copyright, :license, to: :purl_object
+
+  def no_rights_information_present?
+    use_and_reproduction.blank? &&
+      copyright.blank? &&
+      license.blank?
+  end
+
+  def default_attribution
+    'Provided by the Stanford University Libraries'
+  end
+
+  def stanford_libraries_logo_url
+    # NOTE: This is the URL Mirador already uses, so this increases parity between the
+    #       image and media viewers even if it may appear brittle
+    'https://stacks.stanford.edu/image/iiif/wy534zh7137/SULAIR_rosette/full/400,/0/default.jpg'
+  end
 end
