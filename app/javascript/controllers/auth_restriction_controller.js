3ff6755b0d4d84928856b3df205852e851e22e14
Johnathan Martin <john.martin@stanford.edu>
2023-11-29 23:15:04 -0800

    make auth-restriction div / media-component-body div space sharing more consistent
    * start with the auth-restriction div hidden when the DOM is initially rendered
    * un-hide it in the methods that do other DOM manipulation to display auth restriction messages conditionally based on authResponse
    * hide it if the auth-restriction message is dismissed
    * simplify the CSS selector for media-component-body sizing, since now it only has to care about whether auth-restriction div is hidden or not
    * rename a method for accuracy, take advantage of a possible simplification (stimulus auth_restriction_controller.dismissStanfordRestriction becomes .hideLoginPrompt)
    
    otherwise, i found that the old CSS was sometimes subtracting from max-height for media-component-body when it should've been left full height, presumably some things in the more complex selector weren't hidden as expected.

--- app/javascript/controllers/auth_restriction_controller.js
+++ app/javascript/controllers/auth_restriction_controller.js
@@ -31,26 +31,30 @@ export default class extends Controller {
     this.stanfordRestrictionDismissButtonTarget.hidden = false
   }
 
-  dismissStanfordRestriction() {
-    this.stanfordRestrictionTarget.hidden = true
+  hideAuthRestrictionMessages() {
+    this.element.hidden = true
   }
 
   displayStanfordRestriction(loginService) {
+    this.element.hidden = false
     this.stanfordLoginButtonTarget.dataset.mediaTagLoginServiceParam = loginService['@id']
     this.stanfordRestrictionTarget.hidden = false
   }
 
   displayLocationRestriction(restrictionLocation) {
+    this.element.hidden = false
     this.locationRestrictionTarget.querySelector('.loginMessage').innerText = `Access is restricted to the ${restrictionLocation.label}. See Access conditions for more information.`
     this.locationRestrictionTarget.hidden = false
   }
 
   displayEmbargoRestriction(embargo) {
+    this.element.hidden = false
     this.embargoRestrictionTarget.querySelector('.loginMessage').innerText = `Access is restricted until ${this.formattedEmbargo(embargo)}`
     this.embargoRestrictionTarget.hidden = false
   }
 
   displayEmbargoAndStanfordRestriction(embargo, loginService) {
+    this.element.hidden = false
     this.embargoAndStanfordRestrictionTarget.querySelector('.loginMessage').innerText = `Access is restricted to Stanford-affiliated patrons until ${this.formattedEmbargo(embargo)}`
     this.embargoLoginButtonTarget.dataset.mediaTagLoginServiceParam = loginService['@id']
     this.embargoAndStanfordRestrictionTarget.hidden = false
