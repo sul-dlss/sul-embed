724c412c93867af1c0b8c01abfb1dd29118a91e8
Aaron Collier <aaron.collier@stanford.edu>
2023-12-06 09:50:00 -0800

    Switch to stimulus target

--- app/javascript/controllers/locked_audio_poster_controller.js
+++ app/javascript/controllers/locked_audio_poster_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "poster"]
   // Display the waveform when logged out as the audio tag
   // does not support the poster attribute
   show() {
     if (this.isAudio()) {
       const poster_path = this.poster()
       if (poster_path)
-        document.getElementById('audio-poster').setAttribute('src', poster_path)
+        this.posterTarget.setAttribute('src', poster_path)
 
       this.element.hidden = false
     }
@@ -22,8 +23,6 @@ export default class extends Controller {
   }
 
   poster() {
-    const tags = document.querySelectorAll('audio')
-    const audio_posters = [...tags].map((tag) => tag.getAttribute('poster'))
-    return audio_posters.filter(poster => poster.includes('default.jpg'))[0]
+    return document.querySelector('audio').getAttribute('poster')
   }
 }
