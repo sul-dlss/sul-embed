52b1f5e1212d5ff3b97f6470acb2e07bba2242a4
Justin Coyne <jcoyne@justincoyne.com>
2023-12-13 08:19:59 -0600

    Remove useless rule

--- app/javascript/src/modules/media_thumbnail_builder.js
+++ app/javascript/src/modules/media_thumbnail_builder.js
@@ -28,7 +28,7 @@ export default function(dataset, index) {
     const fileLabel = dataset.fileLabel || '';
     // Note: the "position: relative" is required for the stretched-link style.
     return `<li class="media-thumb ${activeClass}" data-controller="thumbnail" data-action="click->thumbnail#activate" data-thumbnail-index-param="${index}" style="position: relative;" aria-controls="sul-embed-body" role="tab">
-        <a class="sul-embed-stretched-link" href="#">
+        <a class="stretched-link" href="#">
           ${thumbnailIcon}
           <span class="${labelClass}">
             ${stanfordOnlyScreenreaderText}${restrictedTextMarkup}
