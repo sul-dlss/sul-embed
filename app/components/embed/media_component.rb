c02fc23138fc83d74256644156920fc8e2e1c578
Justin Coyne <jcoyne@justincoyne.com>
2023-12-14 14:35:11 -0600

    Move the media specific parts to the media component

--- app/components/embed/media_component.rb
+++ app/components/embed/media_component.rb
@@ -7,5 +7,14 @@ module Embed
     end
 
     attr_reader :viewer
+
+    delegate :purl_object, to: :viewer
+    delegate :druid, to: :purl_object
+
+    def resources_with_primary_file
+      @resources_with_primary_file ||= purl_object.contents.select do |purl_resource|
+        purl_resource.primary_file.present?
+      end
+    end
   end
 end
