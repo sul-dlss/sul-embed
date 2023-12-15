68a228aeb4edeffe9e07da2b36efaba2d31a60ee
Justin Coyne <jcoyne@justincoyne.com>
2023-12-15 17:21:19 -0600

    Extract media authorization headers from the companion window
    This auth is specific to the media, so we won't want to keep it in a generic companion window component

--- spec/components/embed/media_component_spec.rb
+++ spec/components/embed/media_component_spec.rb
@@ -3,13 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe Embed::MediaComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    allow(embed_request).to receive(:purl_object).and_return(purl_object)
+    render_inline(described_class.new(viewer:))
+  end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  let(:embed_request) { Embed::Request.new({}) }
+  let(:viewer) do
+    Embed::Viewer::Media.new(embed_request)
+  end
+  let(:purl_object) do
+    instance_double(Embed::Purl,
+                    title: 'foo',
+                    purl_url: 'https://purl.stanford.edu/123',
+                    manifest_json_url: 'https://purl.stanford.edu/123/iiif/manifest',
+                    use_and_reproduction: '',
+                    copyright: '',
+                    license: '',
+                    druid: '123',
+                    contents: [],
+                    downloadable_files: [],
+                    downloadable_transcript_files?: false)
+  end
+
+  it 'displays the page' do
+    # Accessabile dialog
+    within 'dialog' do
+      expect(page).to have_content 'To request a transcript or other accommodation'
+    end
+
+    # Auth components
+    expect(page).to have_content 'Access is restricted to the reading room. See Access conditions for more information.'
+    expect(page).to have_content 'Stanford users: log in to access all available features'
+    expect(page).to have_content 'Access is restricted until the embargo has elapsed'
+  end
 end
