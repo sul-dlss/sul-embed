1360c1bdfb9582d41279ebdf6e325db5f1104820
Justin Coyne <jcoyne@justincoyne.com>
2023-12-19 13:47:27 -0600

    Add tweaks to login process

--- app/javascript/controllers/file_auth_controller.js
+++ app/javascript/controllers/file_auth_controller.js
@@ -7,6 +7,8 @@ export default class extends Controller {
 
   addPostCallbackListener() {
     window.addEventListener("message", (event) => {
+      this.iframe.remove()
+      console.log("Post message", event.data)
       if (event.origin !== "https://stacks.stanford.edu" && event.origin !== "https://sul-stacks-stage.stanford.edu") return;
       if (event.data.type === "AuthAccessTokenError2") {
         this.displayAccessTokenError(event.data)
@@ -59,26 +61,41 @@ export default class extends Controller {
     // We're going to make the assumption that calling the probe without a token is going to fail.
     // So we'll just get the token first.
     console.log(probeService)
+    const accessService = this.findAccessService(probeService)
+
+    const messageId = 'ae3415' // TODO: Make this random
+    this.resources[messageId] = { probeService, contentResourceId }
+  
+    if (accessService.profile === "active") {
+      this.loginNeeded(accessService, messageId)
+    } else {
+      this.initiateTokenRequest(accessService, messageId)
+    }
+  }
+
+  findAccessService(probeService) {
     const accessService = probeService.service.find((service) => service.type === "AuthAccessService2")
 
     if (!accessService)
       throw(`No access service found`)
 
+    return accessService
+  }
+
+  findTokenService(accessService) {
     const tokenService = accessService.service.find((service) => service.type === "AuthAccessTokenService2")
     if (!tokenService)
       throw(`No token service found`)
 
-    if (accessService.profile === "active")
-      this.login(accessService)
-
-    const messageId = 'ae3415' // TODO: Make this random
-    this.resources[messageId] = { probeService, contentResourceId }
-    this.initiateTokenRequest(tokenService, messageId)
+    return tokenService
   }
 
   displayAccessTokenError(accessTokenError) {
     console.error("There was an error getting the token", accessTokenError)
     alert("Authentication error. Unable to get a token from Stacks.")
+    const resource = this.resources[accessTokenError.messageId]
+    const activeAccessService = this.findAccessService(resource.probeService)
+    this.loginNeeded(activeAccessService, accessTokenError.messageId)
   }
 
   accessTokenReceived(token, messageId) {
@@ -98,21 +115,45 @@ export default class extends Controller {
     }
   }
 
-  initiateTokenRequest(tokenService, messageId) {
-    const iframe = document.createElement('iframe')
-    iframe.src = `${tokenService.id}?messageId=${messageId}&origin=${window.origin}`
-    document.body.appendChild(iframe)
+  initiateTokenRequest(accessService, messageId) {
+    const tokenService = this.findTokenService(accessService)
+
+    this.iframe = document.createElement('iframe')
+    this.iframe.src = `${tokenService.id}?messageId=${messageId}&origin=${window.origin}`
+    console.log(`Creating iframe for ${tokenService.id}`)
+    document.body.appendChild(this.iframe)
   }
 
   // Open the login window in a new window and then poll to see if the auth credentials are now active.
-  login(activeAccessService) {
-    const windowReference = window.open(activeAccessService.id);
+  loginNeeded(activeAccessService, messageId) {
+    this.dialog = document.createElement('dialog')
+    this.dialog.innerHTML = `${activeAccessService.label.en[0]}: <button data-action="file-auth#login" data-file-auth-messageId-param="${messageId}" data-file-auth-url-param="${activeAccessService.id}">${activeAccessService.confirmLabel.en[0]}</button>`
+    this.element.appendChild(this.dialog)
+    this.dialog.showModal()
+  }
+
+  login(evt) {
+    this.dialog.remove()
+    console.log(evt)
+    const windowReference = window.open(evt.params.url);
     let loginStart = Date.now();
+    console.log("window reference", windowReference)
     let checkWindow = setInterval(() => {
+      console.log("in interval", (Date.now() - loginStart))
       if ((Date.now() - loginStart) < 30000 &&
         (!windowReference || !windowReference.closed)) return;
 
       clearInterval(checkWindow);
+      // once the window is closed we can initiate the token request
+      this.afterLoginWindowClosed(evt.params.messageid)
     }, 500)
+  }
+
+  afterLoginWindowClosed(messageId) {
+    console.log("Done waiting on that window")
+    const probeService = this.resources[messageId].probeService
+    const accessService = this.findAccessService(probeService)
+
+    this.initiateTokenRequest(accessService, messageId)
   }
 }
