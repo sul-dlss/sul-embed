26d6d3cc71699d6ee23310ae1384ddb9a741a56c
Huda Khan <hudakhan@stanford.edu>
2024-01-11 14:52:43 -0700

    trying out another change

--- app/components/embed/media_tag_component.rb
+++ app/components/embed/media_tag_component.rb
@@ -94,7 +94,8 @@ module Embed
       # We want to enable the user to select from any of these options.
       # We also want the different language options to be listed alphabetically.
       safe_join(
-        @resource.caption_files.map_with_index do |caption_file|
+        @resource.caption_files.map_with_index do |caption_file, i|
+          default_attribute = i.zero? ? 'true' : nil
           tag.track(src: caption_file.file_url, kind: 'captions',
                     srclang: caption_file.language_code, label: caption_file.language_label,
                     default: 'true')
