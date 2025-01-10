ce22e3b66b4e1bd59bf9c6d5c6d61479b672f9e0
dnoneill <noneill@stanford.edu>
2025-01-10 15:40:13 -0500

    refactor download for items with captions/transcripts

--- app/components/embed/companion_windows_component.rb
+++ app/components/embed/companion_windows_component.rb
@@ -38,5 +38,12 @@ module Embed
 
       false
     end
+
+    # We use this in the download FileListComponent to determine if we need to group files.
+    # For example, if a media file has a caption or transcript
+    # we will want to group the caption with the media file.
+    def show_headers?
+      downloadable_files.any? { |file| file.caption? || file.transcript? }
+    end
   end
 end
