cc657b41b6d55dceffd641773766535ab300c476
Justin Coyne <jcoyne@justincoyne.com>
2025-01-31 15:56:01 -0600

    Use i18n for text differences between viewer types

--- app/components/content_list_component.rb
+++ app/components/content_list_component.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class ContentListComponent < ViewComponent::Base
-  def initialize(viewer:, heading:)
+  def initialize(viewer:)
     @viewer = viewer
-    @heading = heading
   end
 
-  attr_reader :viewer, :heading
+  attr_reader :viewer
+
+  def heading
+    t('content_list_heading', scope: viewer.i18n_path)
+  end
 end
