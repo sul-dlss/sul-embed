8d10680472b85f6a49e38fcc4de73b1218843dd7
Justin Coyne <jcoyne@justincoyne.com>
2025-02-07 13:09:56 -0600

    Add fullscreen for File viewer

--- app/components/pdf_component.rb
+++ app/components/pdf_component.rb
@@ -13,4 +13,10 @@ class PdfComponent < ViewComponent::Base
   def iiif_v3_manifest_url
     "#{Settings.purl_url}/#{druid}/iiif3/manifest"
   end
+
+  # chromium has a bug preventing the esc key from exiting full screen in some situations, see
+  # https://issues.chromium.org/issues/40131988 and https://github.com/sul-dlss/sul-embed/issues/2162
+  def requested_by_chromium?
+    %r{(Chrome|Edg)/\d+}.match?(request.headers['User-Agent'])
+  end
 end
