e1c122f16706a42dd26b1db9ee464381aa967b72
Justin Coyne <jcoyne@justincoyne.com>
2025-02-12 17:16:16 -0600

    Set browser width

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,7 @@ require 'view_component/system_test_helpers'
 Capybara.register_driver :selenium_chrome_headless do |app|
   browser_options = Selenium::WebDriver::Chrome::Options.new
   browser_options.add_argument('--headless=new')
+  browser_options.add_argument('--window-size=1920,1080')
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
 
