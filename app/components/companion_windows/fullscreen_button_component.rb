3008ff8393d948cc4ff2b368e371c873f0e2c30d
dnoneill <noneill@stanford.edu>
2025-02-20 17:17:05 -0500

    don't display fullscreen icon on ios devices

--- app/components/companion_windows/fullscreen_button_component.rb
+++ app/components/companion_windows/fullscreen_button_component.rb
@@ -2,5 +2,10 @@
 
 module CompanionWindows
   class FullscreenButtonComponent < ViewComponent::Base
+    def render?
+      # Currently the fullscreen api does not support fullscreen on WebView on iOS
+      # https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API#api.document.fullscreenenabled
+      request.user_agent.exclude?('iPhone')
+    end
   end
 end
