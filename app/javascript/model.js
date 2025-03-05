40c6706be489c9f55943f46324c36bce7803d63f
dnoneill <noneill@stanford.edu>
2025-03-05 18:27:18 -0500

    add login to 3d

--- app/javascript/model.js
+++ app/javascript/model.js
@@ -1,12 +1,10 @@
 import "controllers"
 
-import ModelViewer from "src/modules/model_viewer"
 import { trackView } from "src/modules/metrics"
 
 import "@google/model-viewer"
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("sul-embed-object").hidden = false
-  ModelViewer.init()
   trackView()
 })
