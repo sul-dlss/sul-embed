94f64fc65ed2123cbee85bccc0ef00d70d1db3e1
dnoneill <noneill@stanford.edu>
2025-03-05 15:08:57 -0500

    add check to see if released into earthworks

--- app/components/companion_windows/external_link_component.rb
+++ app/components/companion_windows/external_link_component.rb
@@ -10,5 +10,11 @@ module CompanionWindows
     attr_reader :viewer, :label
 
     delegate :external_url, to: :viewer
+
+    def render?
+      return true unless viewer.instance_of?(::Embed::Viewer::Geo)
+
+      Embed::MetaJson.new(purl_object: viewer.purl_object).earthworks?
+    end
   end
 end
--- spec/components/geo_component_spec.rb
+++ spec/components/geo_component_spec.rb
@@ -8,9 +8,13 @@ RSpec.describe GeoComponent, type: :component do
   end
   let(:viewer) { Embed::Viewer::Geo.new(request) }
   let(:purl) { build(:purl, :geo) }
+  let(:json) { { earthworks: true } }
 
   before do
     allow(Embed::Purl).to receive(:find).and_return(purl)
+
+    stub_request(:get, 'https://purl.stanford.edu/cz128vq0535.meta_json')
+      .to_return(status: 200, body: json.to_json, headers: {})
 
     render_inline(described_class.new(viewer:))
   end
@@ -19,9 +23,18 @@ RSpec.describe GeoComponent, type: :component do
     # visible false because we display:none the container until we've loaded the CSS.
     expect(page).to have_css '.sul-embed-geo', visible: :all
     expect(page).to have_css '#sul-embed-geo-map', visible: :all
+    expect(page).to have_css("a[href='https://earthworks.stanford.edu/catalog/stanford-abc123'] span", text: 'EarthWorks', visible: :all)
     expect(page).to have_css('#sul-embed-geo-map[style="flex: 1"]', visible: :all)
     expect(page).to have_css('#sul-embed-geo-map[data-bounding-box=\'[["-1.478794", "29.572742"], ["4.234077", "35.000308"]]\']', visible: :all)
     expect(page).to have_css('#sul-embed-geo-map[data-wms-url="https://geowebservices.stanford.edu/geoserver/wms/"]', visible: :all)
     expect(page).to have_css('#sul-embed-geo-map[data-layers="druid:cz128vq0535"]', visible: :all)
+  end
+
+  context 'when not released to earthworks' do
+    let(:json) { { earthworks: false } }
+
+    it 'does not show external link' do
+      expect(page).to have_no_css("a[href='https://earthworks.stanford.edu/catalog/stanford-abc123'] span", text: 'EarthWorks', visible: :all)
+    end
   end
 end
