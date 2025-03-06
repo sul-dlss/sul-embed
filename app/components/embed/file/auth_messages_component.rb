074eda404e6ab935781a888704507054f879b352
Justin Coyne <jcoyne@justincoyne.com>
2025-03-06 14:26:09 -0600

    Rename AuthMessagesComponent to show it's only used with the file viewer

---
+++ app/components/embed/file/auth_messages_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Embed
+  module File
+    class AuthMessagesComponent < ViewComponent::Base
+      def initialize(message:)
+        @message = message
+      end
+
+      attr_reader :message
+
+      def icon
+        MESSAGE_ICONS[message[:type]]
+      end
+
+      MESSAGE_ICONS = { 'embargo' => Icons::LockClockComponent,
+                        'stanford' => Icons::LockPersonComponent,
+                        'location-restricted' => Icons::LockGlobeComponent }.freeze
+
+      def render?
+        message.present?
+      end
+    end
+  end
+end
