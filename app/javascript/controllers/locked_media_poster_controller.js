c65ccb3943d2310a94eac153af353433230a483e
dnoneill <noneill@stanford.edu>
2025-08-06 18:03:12 -0400

    allow for a/v material not to have a poster

--- app/javascript/controllers/locked_media_poster_controller.js
+++ app/javascript/controllers/locked_media_poster_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   hide() {
     document.querySelectorAll('video').forEach(video => {
-      if(video.getAttribute("poster").includes('locked'))
+      if(video.getAttribute("poster")?.includes('locked'))
         video.removeAttribute("poster")
     })
   }
