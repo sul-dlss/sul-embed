06638560e8ed246f95ce9fa52bece30cbaecd118
Marlo Longley <mlongley@stanford.edu>
2025-08-13 17:28:53 -0400

    Refactor Mirador-related Javascript organization

---
+++ app/javascript/src/mirador/plugins/embedModePlugin.js
@@ -0,0 +1,33 @@
+import CompareDialog from '@/mirador/components/CompareDialog.jsx';
+import CompareMenuItem from '@/mirador/components/CompareMenuItem.jsx';
+import AllowTopBarClose from '@/mirador/components/AllowTopBarClose.jsx';
+import AllowDragAndDrop from '@/mirador/components/AllowDragAndDrop.jsx';
+import { addWindow, getWindow, updateWindow } from 'mirador';
+
+export default [
+  {
+    component: AllowTopBarClose,
+    mode: 'wrap',
+    target: 'WindowTopBar',
+  },
+  {
+    component: AllowDragAndDrop,
+    mode: 'wrap',
+    target: 'Workspace',
+  },
+  {
+    component: CompareMenuItem,
+    mapDispatchToProps: { updateWindow },
+    mode: 'add',
+    target: 'WindowTopBarPluginMenu',
+  },
+  {
+    component: CompareDialog,
+    mapDispatchToProps: { addWindow, updateWindow },
+    mapStateToProps: (state, { windowId }) => ({
+      show: getWindow(state, { windowId })?.showCompareDialog,
+    }),
+    mode: 'add',
+    target: 'Window',
+  },
+];
