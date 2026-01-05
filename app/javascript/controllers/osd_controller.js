74129a627cd3a8aaddb861b49e76a80e7fea442b
dnoneill <noneill@stanford.edu>
2026-01-05 15:52:34 -0500

    allow for images to be first in content list for a/v materials

--- app/javascript/controllers/osd_controller.js
+++ app/javascript/controllers/osd_controller.js
@@ -6,6 +6,20 @@ export default class extends Controller {
     url: String,
     navImages: Object,
   }
+  connect() {
+    // Images are still on IIIF Auth V1 which does not include a probe service (V2 only).
+    // file-auth-controller expects a probe service response for the first item in the manifest which isn't set up for images.
+    // This will fix this issue until we get IIIF Auth V2 set up for images which is waiting on Mirador to support Auth V2.
+    // https://github.com/ProjectMirador/mirador/issues/3789
+
+    // setTimeout is needed, because without it, the viewer initializes before the drawer fully opens and the zoom is off.
+    if (this.element.dataset.index == 0){
+      setTimeout(() => {
+        const event = new CustomEvent('thumbnail-clicked', { detail: { index: 0, fileUri: this.urlValue } })
+        this.initializeViewer(event)
+      }, 500);
+    }
+  }
 
   initializeViewer(evt) {
     // Customize the error message displayed to users:
